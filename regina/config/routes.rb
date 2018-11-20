0362299a649cfee1ef587c8cbda576093a121a63
armi <armi112653@gmail.com>
2018-11-20 23:53:48 +0800

    Mac : Route

--- regina/config/routes.rb
+++ regina/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+  get '/5x',to: redirect('https://baidu.com')
   resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
