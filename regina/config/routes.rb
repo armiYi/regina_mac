a4c0f51e8bca4789ad10fe04d0914149a3ccb43f
armi <armi112653@gmail.com>
2018-11-26 00:09:15 +0800

    Mac : Condidate CRUD

--- regina/config/routes.rb
+++ regina/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post 'bmi/result',to: 'bmi#result'
   resources :posts
   resources :users
+  resources :candidates
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
