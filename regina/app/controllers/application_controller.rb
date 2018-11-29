503db38baa861572f204d27e6101221a81dc8700
armi <armi112653@gmail.com>
2018-11-30 00:14:20 +0800

    Mac : Model

--- regina/app/controllers/application_controller.rb
+++ regina/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
     def hello
         render html: "hello, world!"
     end
+
+    rescue_from ActiveRecord::RecordNotFount,with: :record_not_found
+
+    private
+    def record_not_found
+        render plain: '查无资料',status: 404
+    end
+
 end
